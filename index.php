81e7ace7d1eb4518aa0487a95d12192a4d019451
disana <disanaberrospinieto5@gmail.com>
2025-12-03 10:35:21 -0500

    4to cambio 03/12/2025

--- index.php
+++ index.php
@@ -7,5 +7,14 @@
 </head>
 <body>
    <h1>mi primer repositorio en github</h1> 
+   <p>este contenidocde mi primer repositorio en github.</p>
+   <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Acerca de</a></li>
+            <li><a href="contact.php">Contacto</a></li>
+        </ul>
+    </nav>
+    <h3> ya esta quedando listo</h3>
 </body>
 </html>
